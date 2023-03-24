0b110a8b6f36debbc681374a4be001149d610017
Jake Butters <jacobbutters@gmail.com>
2023-03-24 11:42:13 -0600

    cv page complete

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../../components/CV.css';
 import cv from "../documents/Thomas-Kelemen-CV.docx"
-import books from "../image/books.jpg"
+import openBooks from "../image/open_books.jpg";
 
 const CV = () => {
-
-  return <div className='cv_container'>
-
-    <p>Please click <u><a href={cv} download='THomas-Kelemen-CV'>here</a></u> to download my CV</p>
-
-    <img className='cv_books' src={books} />
-
-  </div>
+  return (
+    <div className='cv_container'>
+      <img className='cv_image' src={openBooks} />
+      <div className='cv_info'>
+        <p><center>Dr. Thomas Kelemen's Curriculum Vitae</center></p>
+        <a className='cv_download_btn' href={cv} download='THomas-Kelemen-CV'>Download a copy of my C.V.</a>
+      </div>
+    </div>)
 }
 
 export default CV;
